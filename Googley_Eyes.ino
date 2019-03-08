49b48fff7d1b3f06a8b52a09f21c94200f5d9db3
smokeandlights <smokeandlights@hotmail.com>
2019-03-08 12:23:04 -0500

    Add files via upload

---
+++ Googley_Eyes.ino
@@ -0,0 +1,36 @@
+#include <WEMOS_Matrix_LED.h>
+
+MLED mled(5); //set intensity=5
+
+void setup() {
+  // put your setup code here, to run once:
+
+  
+}
+
+void loop() {
+int pupilXpos = 3 //pupil position. I want to map this to a joystick or other control later
+int pupilYpos = 3
+
+for(int x = 2; x < 5; x++){        //Drawing the eye ball
+   for(int y = 0; y < 7; y++){
+      mled.dot(x , y);
+      }
+   }
+for(int y=1;y<6;y++){
+    mled.dot(1,y);
+    mled.dot(6,y);
+}
+for(int y=2;y<5;y++){
+    mled.dot(0,y);
+    mled.dot(7,y);
+} 
+
+mled.dot(pupilXpos,pupilYpos,0); //drawing the pupil
+mled.dot((pupilXpos+1),pupilYpos,0);
+mled.dot(pupilXpos,(pupilYpos+1),0);
+mled.dot((pupilXpos+1),(pupilYpos+1),0);
+
+
+mled.display(); //refresh the display
+}
